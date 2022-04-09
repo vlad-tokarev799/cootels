076e6dcc1ba437fb07af278a643a8beef26d3f7d
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2022-04-09 14:56:39 +0800

    added faq block and animations

---
+++ src/assets/js/components/questions.js
@@ -0,0 +1,11 @@
+const questionsBtns = document.querySelectorAll('.question__btn')
+
+if (questionsBtns.length) {
+    questionsBtns.forEach(item => {
+        item.onclick = function() {
+            const parentNode = this.closest('.question')
+
+            parentNode.classList.toggle('question_open')
+        }
+    })
+}
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,9 @@
 //= plugins/jquery-3.6.0.js
+//= plugins/wow.js
 
 //= components/burgerMenu.js
+//= components/questions.js
+
+new WOW({
+    offset: 100
+}).init()
